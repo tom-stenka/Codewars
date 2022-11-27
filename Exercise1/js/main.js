98996f1f7b2078282f87d698eaf0d98718679710
Tomasz Stenka <tom.stenka@gmail.com>
2022-11-27 14:10:47 +0100

    gravity_flip task solved

---
+++ Exercise1/js/main.js
@@ -0,0 +1,33 @@
+/* DESCRIPTION */
+
+/*All of the animals are having a feast! Each animal is bringing one dish. There is just one rule: the dish must start and end with the same letters as the animal's name. For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
+
+Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
+
+Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals. */
+
+/* Strings, fundamentals */
+
+/*My solution*/
+
+function feast(beast, dish) {
+    const feastCheck = (beast.charAt(0) + beast.charAt(beast.length-1) == dish.charAt(0) + dish.charAt(dish.length-1)) ? true : false;
+    return feastCheck;
+}
+
+console.log(feast("great blue heron", "garlic naan"));
+
+
+/*others solutions (most "clever" points)*/ 
+
+function feast1(beast, dish) {
+	return beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]
+}
+
+function feast2(beast, dish) {
+    return beast[0]===dish[0] && beast.slice(-1)===dish.slice(-1);
+}
+
+function feast3(beast, dish) {
+    return dish.startsWith(beast[0]) && dish.endsWith(beast[beast.length-1]);
+}
